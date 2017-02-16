bf567f1beec4ec7ef89f2be61124b4158413b3a2
Niels <niels@w3b.net>
2017-02-16 22:59:56 +0100

    Insert WebSocket

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const
   https = require('https'),
+  ws = require('websocket').server,
   fs = require('fs'),
   options = {
     key: fs.readFileSync('k.pem'),
     cert: fs.readFileSync('c.pem')
   };
 
-https.createServer(options, function(req, res) {
+const
+  server = https.createServer(options, function(req, res) {
   res.writeHead(200);
   fs.readFile('public/index.html', 'utf8', function (err, data) {
     if (err) {
@@ -14,4 +16,32 @@ https.createServer(options, function(req, res) {
     }
     res.end(data);
   });
-}).listen(443);
+})
+
+server.listen(443, function() {
+  console.log((new Date()) + ' Server started at port 443');
+});
+
+wsServer = new ws({
+  httpServer: server,
+  autoAcceptConnections: true,
+  rejectUnauthorized: false
+});
+
+wsServer.on('request', function(request) {
+  const
+    connection = request.accept('echo-protocol', request.origin);
+  console.log((new Date()) + ' Connection accepted');
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ' + message.utf8Data);
+      connection.sendUTF(message.utf8Data);
+    } else if (message.type === 'binary') {
+      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+      connection.sendBytes(message.binaryData);
+    }
+  });
+  connection.on('close', function(reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
+});
